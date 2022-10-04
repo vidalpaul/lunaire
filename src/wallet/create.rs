5fce84c9a1679c9f375696e1f098e4d851c31fe2
Paulo Vidal <u1d4lp@gmail.com>
2022-10-04 16:10:17 -0300

    update/Cargo deps

---
+++ src/wallet/create.rs
@@ -0,0 +1,10 @@
+pub struct WalletCreationObject {
+    pub network: String,
+    pub mnemonic: String,
+    pub passphrase: String,
+    pub xprv: String,
+    pub xpub: String,
+    pub address: String,
+    pub wif: String,
+    pub pin: String,
+}
